4054f573984d326c13e4524fd15a420f7218c803
Mitsunobu Yoneda <90941863+higamaya@users.noreply.github.com>
2021-10-03 12:34:48 +0900

    Add sidenav & Improve smooth scroll

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,7 +1,25 @@
-(function($){
-	$(function(){
+(function($) {
+	// Setup Materialize CSS
+	$(function() {
 		$('.sidenav').sidenav();
-		$('.parallax').parallax();
 		$('.scrollspy').scrollSpy();
+		$('.parallax').parallax();
+	});
+	// Setup smooth scroll for in-page links
+	$(function() {
+		$('a[href^="#"]').on('click', function() {
+			let href= $(this).attr('href');
+			let target = $(href == '#' || href == '' ? 'html' : href);
+			if (target.length > 0) {
+				M.anime({
+					targets: [document.documentElement, document.body],
+					scrollTop: target.offset().top - $('.navbar-fixed').height(),
+					duration: 400,
+					easing: 'easeInCubic'
+				});
+				M.Sidenav.getInstance($('.sidenav')).close();
+				return false;
+			}
+		});
 	});
 })(jQuery);
